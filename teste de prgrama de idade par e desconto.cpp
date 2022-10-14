b0db4db554a2f5a2e7e79fbc10346ee501036d3e
Andre-nemesis <105876843+Andre-nemesis@users.noreply.github.com>
2022-10-14 17:00:47 -0300

    Add files via upload

---
+++ teste de prgrama de idade par e desconto.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale>
+int main() {
+	setlocale(LC_ALL,"Portuguese_Brazil");
+	
+	int idade1,idade2;
+	
+	printf("Idade 1\n");
+	scanf("%d",&idade1);
+	printf("Idade 2\n");
+	scanf("%d",&idade2);
+	
+	if (idade1 % 2 == 0){
+		printf("Você recebeu desconto");
+	}	else {
+		printf("Você não recebeu desconto;-;");
+	}
+	
+	return 0;
+}
